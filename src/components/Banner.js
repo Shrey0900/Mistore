6f4d718ee56d52de8538dc11964545b930b7b3ef
Shrey0900 <shreychandola98@gmail.com>
2023-06-17 13:42:45 +0530

    Youtube initial

---
+++ src/components/Banner.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Carousel from "react-bootstrap/Carousel"
+import "bootstrap/dist/css/bootstrap.min.css"
+
+const Banner = ({end}) => {
+  return (
+    <Carousel fade>
+        {end.map((item,index)=>(
+            <Carousel.Item key={item.image} id="banner" interval={1000} keyboard={true}>
+                <img src={item.image} alt={`${index} banner`} id="bannerImage" className='d-block w-100' />
+                <Carousel.Caption>
+                    <h3>{item.name}</h3>
+                    <p>{item.description}</p>
+                    <p>{item.source}</p>
+                    <u>Read more</u>
+                </Carousel.Caption>
+            </Carousel.Item>
+        ))}
+    </Carousel>
+  )
+}
+
+export default Banner
---
+++ src/components/HotAccessories.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import HotItemCard from "./HotItemCard.js"
+import '../styles/HotAccessories.css'
+
+const HotAccessories = ({music,musicCover,smartDevice,smartDeviceCover,home,homeCover,lifestyle,lifestyleCover,mobileAccessories,mobileAccessoriesCover}) => {
+  return (
+    <div className='HotAccessories'>
+      <div>
+        <img src={musicCover || smartDeviceCover || homeCover || lifestyleCover || mobileAccessoriesCover} alt="Cover" />
+      </div>
+      <div>
+        {music && music.map((item,index)=>(
+            <HotItemCard key={item.image} name={item.name} price={item.price} image={item.image} index={item.index} />
+        ))}
+        {smartDevice && smartDevice.map((item,index)=>(
+            <HotItemCard key={item.image} name={item.name} price={item.price} image={item.image} index={item.index} />
+        ))}
+        {home && home.map((item,index)=>(
+            <HotItemCard key={item.image} name={item.name} price={item.price} image={item.image} index={item.index} />
+        ))}
+        {lifestyle && lifestyle.map((item,index)=>(
+            <HotItemCard key={item.image} name={item.name} price={item.price} image={item.image} index={item.index} />
+        ))}
+        {mobileAccessories && mobileAccessories.map((item,index)=>(
+            <HotItemCard key={item.image} name={item.name} price={item.price} image={item.image} index={item.index} />
+        ))}
+        <HotItemCard image="https://i01.appmifile.com/webfile/globalimg/in/cms/593181BE-7DB8-15F5-E907-506427B8C9F4.jpg"/>
+      </div>
+    </div>
+  )
+}
+
+export default HotAccessories
